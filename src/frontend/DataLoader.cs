c6d372941d76258e4b922614e14cfde06119e2f8
Siddhartha Gandhi <siddhartha.a.gandhi@gmail.com>
2019-11-30 11:40:39 -0500

    Change test loan parameters

--- src/frontend/DataLoader.cs
+++ src/frontend/DataLoader.cs
@@ -104,28 +104,22 @@ namespace frontend
 
         public override Deal LoadDeal(string dealName)
         {
-            
-
-
             var deal = new Deal(dealName);
-            for (int i = 1; i < 60; i++)
-            {
-                Loan loan = new Loan(
-                    loanId: "1717469130",
-                    originalBalance: 763000.0,
-                    currentBalance: 763000.0,
-                    firstPaymentDate: new DateTime(2019, 2, 1).AddMonths(-i),
-                    factorDate: new DateTime(2019, 2, 1),
-                    originalLoanTerm: 60,
-                    originalAmortTerm: 360,
-                    originalIOTerm: 0,
-                    grossCoupon: 0.0496,
-                    feeStrip: 0.0248,
-                    accrualBasis: AccrualBasis.ACTUAL_360,
-                    originalPrepaymentString: "YM(54) O(6)"
-                );
-                deal.AddLoanToDeal(loan);
-            }
+            Loan loan = new Loan(
+                loanId: "1717469130",
+                originalBalance: 1_000_000.0,
+                currentBalance: 1_000_000.0,
+                firstPaymentDate: new DateTime(2019, 1, 1),
+                factorDate: new DateTime(2019, 1, 1),
+                originalLoanTerm: 120,
+                originalAmortTerm: 120,
+                originalIOTerm: 0,
+                grossCoupon: 0.06,
+                feeStrip: 0.0,
+                accrualBasis: AccrualBasis.THIRTY_360,
+                originalPrepaymentString: "O(120)"
+            );
+            deal.AddLoanToDeal(loan);
             return deal;
         }
     }
